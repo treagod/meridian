8a48cd85e6811657195d12b7b30b7e1a4c46d393
Marvin Ahlgrimm <marv.ahlgrimm@gmail.com>
2026-03-30 15:45:21 +0200

    Implement cli scaffold

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,10 @@
 require "spec"
 require "../src/meridian"
+
+record CLIResult, output : String, exit_code : Int32
+
+def run_cli(args : Array(String)) : CLIResult
+  io = IO::Memory.new
+  exit_code = Meridian::CLI.run(args, output: io, error: io)
+  CLIResult.new(output: io.to_s, exit_code: exit_code)
+end
